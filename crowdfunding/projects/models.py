b7b0917adc7b5cb1d821bd3bca987a2239c0e5db
tara0d <114784106+tara0d@users.noreply.github.com>
2023-01-19 20:02:39 +1100

    Class work on second Tuesday
    Following the 'Users' PDF on Thinkific

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -9,7 +10,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True) 
-    owner = models.CharField(max_length=200) #Will need to update this to a foreign key to link to other model in database
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -20,4 +25,8 @@ class Pledge(models.Model):
         on_delete=models.CASCADE,       #This is important for when projects are deleted. If model(project) is cancelled, all pledges are deleted
         related_name='pledges'
     )
-    supporter = models.CharField(max_length=200)    #Will need to be updated when user model created
+    supporter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
