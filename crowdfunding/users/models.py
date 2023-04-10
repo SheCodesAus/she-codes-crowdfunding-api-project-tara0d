f97be940ecd03038dcfb5bf23d7fabf12217fff0
tara0d <114784106+tara0d@users.noreply.github.com>
2023-04-10 23:18:08 +1000

    Removed unnecessary fields
    Phone number and bio remove temporarily while not in use.

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -6,9 +6,9 @@ class CustomUser(AbstractUser):
     email = models.EmailField()
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=10)
+    # phone_number = models.CharField(max_length=10)
     developer = models.BooleanField(default=False)    #Need to add way for admin to make created accounts developers ones
-    bio = models.CharField(max_length=300)
+    # bio = models.CharField(max_length=300)
     is_creator = models.BooleanField(default=False)
 
     def __str__(self):
--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -3,10 +3,10 @@ from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
-    phone_number = serializers.RegexField("[0-9]{10}", required=True)
+    # phone_number = serializers.RegexField("[0-9]{10}", required=True)
     class Meta:
         model = CustomUser
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'username', 'phone_number', 'bio', 'is_creator',]
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'username', 'is_creator',]
         read_only_fields = ['id', 'email',]
         extra_kwargs = {'password': {'write_only': True}}
     
